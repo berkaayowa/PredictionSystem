f67d3a58b60ff75fecdb1ad794370ee22c3d5152
berka <ayowaberka@gmail.com>
2023-08-19 03:13:28 +0200

    live score

--- PredictionUI/Views/Client/Pages/index.php
+++
@@ -1,4 +0,0 @@
-<!-- LIVESCORE WIDGET SOCCERSAPI.COM -->
-<div id="ls-widget" data-w="w14638_64e0092a2488c" class="livescore-widget"></div>
-<script type="text/javascript" src="https://ls.soccersapi.com/widget/res/w14638_64e0092a2488c/widget.js"></script>
-<!-- LIVESCORE WIDGET SOCCERSAPI.COM -->
