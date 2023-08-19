2980cf71beea947ac87898bbfef0392a0c957ca7
berka <ayowaberka@gmail.com>
2023-08-19 03:45:13 +0200

    live score

--- PredictionUI/Views/Client/Pages/livescore.php
+++ PredictionUI/Views/Client/Pages/livescore.php
@@ -8,42 +8,64 @@
                         <script type="text/javascript" src="https://ls.soccersapi.com/widget/res/w14638_64e0092a2488c/widget.js"></script>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-3">
-                            <div class="adLeft">
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
-                                        crossorigin="anonymous"></script>
-                                <ins class="adsbygoogle"
-                                     style="display:block"
-                                     data-ad-format="autorelaxed"
-                                     data-ad-client="ca-pub-1836789549483504"
-                                     data-ad-slot="2811558335"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
-                            <div class="adLeft">
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
-                                        crossorigin="anonymous"></script>
-                                <ins class="adsbygoogle"
-                                     style="display:block"
-                                     data-ad-format="autorelaxed"
-                                     data-ad-client="ca-pub-1836789549483504"
-                                     data-ad-slot="3327381899"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
-                            <div class="adLeft">
-                                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
-                                        crossorigin="anonymous"></script>
-                                <ins class="adsbygoogle"
-                                     style="display:block"
-                                     data-ad-format="autorelaxed"
-                                     data-ad-client="ca-pub-1836789549483504"
-                                     data-ad-slot="6527689480"></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle || []).push({});
-                                </script>
-                            </div>
+                        <div class="adLeft">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                    crossorigin="anonymous"></script>
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-format="autorelaxed"
+                                 data-ad-client="ca-pub-1836789549483504"
+                                 data-ad-slot="2811558335"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <div class="adLeft">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                    crossorigin="anonymous"></script>
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-format="autorelaxed"
+                                 data-ad-client="ca-pub-1836789549483504"
+                                 data-ad-slot="3327381899"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <div class="adLeft">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                    crossorigin="anonymous"></script>
+                            <ins class="adsbygoogle"
+                                 style="display:block"
+                                 data-ad-format="autorelaxed"
+                                 data-ad-client="ca-pub-1836789549483504"
+                                 data-ad-slot="6527689480"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <div class="adLeft">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                    crossorigin="anonymous"></script>
+                            <ins class="adsbygoogle"
+                                 style="display:inline-block;width:360px;height:140px"
+                                 data-ad-client="ca-pub-1836789549483504"
+                                 data-ad-slot="7649199465"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                        <div class="adLeft">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                    crossorigin="anonymous"></script>
+                            <ins class="adsbygoogle"
+                                 style="display:inline-block;width:360px;height:140px"
+                                 data-ad-client="ca-pub-1836789549483504"
+                                 data-ad-slot="1994118923"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
                     </div>
                 </div>
 
