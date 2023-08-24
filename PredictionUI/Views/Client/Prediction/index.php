393f863cb4fffb1dbe6ad8e9fd24a4d4d701e29d
berka <ayowaberka@gmail.com>
2023-08-25 00:35:40 +0200

    updated prediction page

--- PredictionUI/Views/Client/Prediction/index.php
+++ PredictionUI/Views/Client/Prediction/index.php
@@ -456,6 +456,21 @@
                             (adsbygoogle = window.adsbygoogle || []).push({});
                         </script>
 
+                        <br>
+
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1836789549483504"
+                                crossorigin="anonymous"></script>
+                        <!-- Home Vertical Ads Unit 2 -->
+                        <ins class="adsbygoogle"
+                             style="display:block"
+                             data-ad-client="ca-pub-1836789549483504"
+                             data-ad-slot="4272807642"
+                             data-ad-format="auto"
+                             data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
+
                     </div>
                 </div>
             </div>
