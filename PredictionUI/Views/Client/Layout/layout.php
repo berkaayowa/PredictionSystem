da13c81d4bb5727cee80e0689be39e87926001ce
berka <ayowaberka@gmail.com>
2023-11-14 12:36:33 +0200

    added cleverwebserver

--- PredictionUI/Views/Client/Layout/layout.php
+++ PredictionUI/Views/Client/Layout/layout.php
@@ -257,6 +257,34 @@
             </div>
         </div>
     </div>
+
+    <script data-cfasync="false" type="text/javascript" id="clever-core">
+        /* <![CDATA[ */
+        (function (document, window) {
+            var a, c = document.createElement("script"), f = window.frameElement;
+
+            c.id = "CleverCoreLoader75333";
+            c.src = "https://scripts.cleverwebserver.com/4ed6c969ccce5e4ceec239a2bdf7482a.js";
+
+            c.async = !0;
+            c.type = "text/javascript";
+            c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+            c.setAttribute("data-callback", "put-your-callback-function-here");
+            c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
+            c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
+
+            try {
+                a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+            } catch (e) {
+                a = !1;
+            }
+
+            a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+            a.parentNode.insertBefore(c, a);
+        })(document, window);
+        /* ]]> */
+    </script>
+
 </body>
 
 </html>
